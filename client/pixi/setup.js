cacabd081b7dabd30015fb846d5f5d7a0280db20
Justin Mattos <justin.mattos@nyu.edu>
2021-05-12 23:13:06 -0400

    set up game loop that moves snake automatically

--- client/pixi/setup.js
+++ client/pixi/setup.js
@@ -6,23 +6,32 @@ import { renderSprite } from './renderSprite';
 export const setup = () => {
   const player = createSnake();
   renderSprite(player);
-  chainMoves(player, [
-    'RIGHT',
-    'RIGHT',
-    'DOWN',
-    'RIGHT',
-    'RIGHT',
-    'UP',
-    'LEFT',
-    'LEFT',
-    'UP',
-    'UP',
-    'LEFT',
-    'DOWN',
-    'DOWN',
-    'DOWN',
-    'DOWN',
-    'DOWN',
-    'DOWN',
-  ]);
+  let direction = 'RIGHT';
+  let speed = 30;
+  let elapsedTime = 0;
+  let running = true;
+  const gameloop = (delta) => {
+    elapsedTime++;
+    if (running) {
+      if (elapsedTime % speed === 0) {
+        moveSprite(player, direction);
+        if (elapsedTime === 30) {
+          direction = 'UP';
+        }
+        if (player.head.y === 40) {
+          direction = 'LEFT';
+          if (player.head.x === 40) {
+            direction = 'DOWN';
+          }
+        }
+        if (player.head.y === 760) {
+          direction = 'RIGHT';
+          if (player.head.x === 760) {
+            direction = 'UP';
+          }
+        }
+      }
+    }
+  };
+  app.ticker.add((delta) => gameloop(delta));
 };
