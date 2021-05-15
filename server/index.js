1e2b1ad476a2fdc2c122a09b648f5e7829d39f6a
Justin Mattos <justin.mattos@nyu.edu>
2021-05-15 12:41:45 -0400

    removed db connection

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 3000;
 
 const { db } = require('./db/index');
 
-db.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log(`App Listening on PORT: ${PORT}`));
-});
+// db.sync({ force: true }).then(() => {
+app.listen(PORT, () => console.log(`App Listening on PORT: ${PORT}`));
+// });
